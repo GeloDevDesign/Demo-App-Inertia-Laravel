03af4243a4ed3465422acfdb52c777a4bda4efbe
Angelo <abserenuela@ccc.edu.ph>
2025-01-15 22:21:39 +0800

    asdfasdfasd

--- routes/auth.php
+++ routes/auth.php
@@ -1,8 +1,6 @@
 <?php
 
 use App\Http\Controllers\AuthController;
-use App\Http\Controllers\HomeController;
-use App\Models\User;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
--- routes/web.php
+++ routes/web.php
@@ -1,29 +1,11 @@
 <?php
 
-use App\Http\Controllers\AuthController;
 use App\Http\Controllers\HomeController;
-use App\Models\User;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
-
-/*
-|--------------------------------------------------------------------------
-| Authentication Routes
-|--------------------------------------------------------------------------
-*/
-
-// Login route
-
-
-/*
-|--------------------------------------------------------------------------
-| Public Routes
-|--------------------------------------------------------------------------
-*/
 
 // Home page route
 Route::get('/', [HomeController::class,'index'])->name('home')->middleware('auth');
 Route::inertia('/welcome', 'Views/Welcome')->name('welcome')->middleware('auth');
-
 
 require __DIR__ . '/auth.php';
